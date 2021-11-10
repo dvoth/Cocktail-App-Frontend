77e8e7aced2f42e940c855389d2266d5a2048cb8
Dalton Voth <voth321@gmail.com>
2021-11-09 21:02:41 -0600

    Add default text if no recipes are available

--- components/screens/RecipeList.js
+++ components/screens/RecipeList.js
@@ -34,30 +34,34 @@ const RecipeList = props => {
         })
         return ingredientList;
     }
-    return (
-        <SafeAreaView>
-            <FlatList
-                data={recipeData.recipes}
-                keyExtractor={({ id }, index) =>  String(id) }
-                renderItem={({ item }) => (
-                    <Pressable style={styles.recipeCard} onPress={() => props.navigation.navigate('Recipe', {recipe: item})}>
-                        <Image 
-                            source={{uri: item.image }} 
-                            style={styles.thumbnail}/>
-                        <View style={styles.recipeDetailsContainer}>
-                                <View style={styles.recipeTitleContainer}>
-                                    <Text style={styles.recipeTitle}>{item.name}</Text>
-                                    <Icon name='favorite-border' size={20}/>
-                                </View>
-                                <Text style={styles.recipeDetails}>8%</Text>
-                                <Text style={styles.recipeDetails}>{getListableIngredients(item)}</Text>
-                                <Text style={styles.recipeDetails}>{item.description}</Text>
-                        </View>
-                    </Pressable>
-                )}/>
-        </SafeAreaView>
-  );
-    
+
+    if (isAuthenticated && recipeData.recipes.length == 0) {
+        return (<Text>Add some ingredients to see recipes you can make!</Text>)
+    } else {
+        return (
+            <SafeAreaView>
+                <FlatList
+                    data={recipeData.recipes}
+                    keyExtractor={({ id }, index) =>  String(id) }
+                    renderItem={({ item }) => (
+                        <Pressable style={styles.recipeCard} onPress={() => props.navigation.navigate('Recipe', {recipe: item})}>
+                            <Image 
+                                source={{uri: item.image }} 
+                                style={styles.thumbnail}/>
+                            <View style={styles.recipeDetailsContainer}>
+                                    <View style={styles.recipeTitleContainer}>
+                                        <Text style={styles.recipeTitle}>{item.name}</Text>
+                                        <Icon name='favorite-border' size={20}/>
+                                    </View>
+                                    <Text style={styles.recipeDetails}>8%</Text>
+                                    <Text style={styles.recipeDetails}>{getListableIngredients(item)}</Text>
+                                    <Text style={styles.recipeDetails}>{item.description}</Text>
+                            </View>
+                        </Pressable>
+                    )}/>
+            </SafeAreaView>
+        );
+    }
 }
 
 export default RecipeList;
